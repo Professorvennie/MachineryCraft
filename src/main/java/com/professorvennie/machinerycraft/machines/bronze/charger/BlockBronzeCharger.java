8c21296f2e53045ac199871653c78151c9a24748
Professorvennie <Prob.vennie@gmail.com>
2014-10-04 20:20:29 -0400

    charger textures

--- src/main/java/com/professorvennie/machinerycraft/machines/bronze/charger/BlockBronzeCharger.java
+++ src/main/java/com/professorvennie/machinerycraft/machines/bronze/charger/BlockBronzeCharger.java
@@ -9,11 +9,14 @@
  * */
 package com.professorvennie.machinerycraft.machines.bronze.charger;
 
+import com.professorvennie.machinerycraft.api.book.BookEntry;
 import com.professorvennie.machinerycraft.lib.LibGuiIds;
 import com.professorvennie.machinerycraft.lib.Names;
 import com.professorvennie.machinerycraft.lib.Reference;
 import com.professorvennie.machinerycraft.machines.bronze.BlockBasicSteamMachine;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -30,6 +33,11 @@ public class BlockBronzeCharger extends BlockBasicSteamMachine {
     @Override
     public TileEntity createNewTileEntity(World world, int i) {
         return new TileEntityBronzeCharger();
+    }
+
+    @Override
+    public BookEntry getEntry(World world, int x, int y, int z, EntityPlayer player, ItemStack book) {
+        return super.getEntry(world, x, y, z, player, book);
     }
 
     @Override
